db44990d0748e44cc396eaac3aa1cfd112b62732
diversunny <gerson@gerson.at>
2018-06-15 00:23:40 +0200

    i18n

--- ejb-test/ejb-test-war/src/main/java/com/gerson/StatelessSingleton.java
+++ ejb-test/ejb-test-war/src/main/java/com/gerson/StatelessSingleton.java
@@ -1,21 +1,27 @@
 package com.gerson;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.ResourceBundle.Control;
+
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
-import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
+
+import com.gerson.i18n.URLPropertiesResourceBundleControlProvider;
 
 @Singleton
 @Startup
 public class StatelessSingleton {
-	@EJB(lookup="java:global/ejb-test/test-ejb/Service!com.gerson.IRemoteService")
+	@EJB(lookup="java:global/ejb-test-ear/test-ejb/Service!com.gerson.IRemoteService")
 	IService service;
 	@PostConstruct
 	public void start() {
+	 
 		try {
 			System.out.println("getHello:"+service.getHello());
 		} catch (Exception e) {
@@ -73,12 +79,31 @@ public class StatelessSingleton {
 		} catch (Exception e) {
 			System.out.println("getHellowResult: failed =>" +e.getMessage());
 		}
-
-		
-		
+		String url="https://qs.ifco-online.com/ResourcesWeb/resources/properties/";
+   	 URL[] urls;
+//	try {
+//		urls = new URL[] {new URL(url)};
+//		
+//		 URLClassLoader urlClassloader=new URLClassLoader(urls,Thread.currentThread().getContextClassLoader());	   
+//		 ResourceBundle rb=ResourceBundle.getBundle("MessageResources", Locale.GERMAN,urlClassloader);
+//			System.out.println("navi.abholung_neu: " +rb.getString("navi.abholung_neu"));
+//	} catch (MalformedURLException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	} 
+//   
+//		
+//		 URLPropertiesResourceBundleControlProvider provider=new URLPropertiesResourceBundleControlProvider();
+//		 Control control = provider.getControl("MessageResources");
+//		ResourceBundle rb=ResourceBundle.getBundle("MessageResources", Locale.ENGLISH,control);
+//		System.out.println("navi.abholung_neu: " +rb.getString("navi.abholung_neu"));
+//		
+		ResourceBundle rb2=ResourceBundle.getBundle("MessageResources", Locale.GERMAN);
+		System.out.println("navi.abholung_neu: " +rb2.getString("navi.abholung_neu"));
+//		
 	}
-	@Produces
-	public static IRemoteService getService() throws NamingException {		
-		return InitialContext.doLookup("java:global/ejb-test/test-ejb/Service!com.gerson.IRemoteService");
-	}
+//	@Produces
+//	public static IRemoteService getService() throws NamingException {		
+//		return InitialContext.doLookup("java:global/ejb-test/test-ejb/Service!com.gerson.IRemoteService");
+//	}
 }
